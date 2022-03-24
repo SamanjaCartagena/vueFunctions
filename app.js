19158efd3cb4322409e8ee601f2c1491c1bef056
SamanjaCartagena <cart.samanja09@gmail.com>
2022-03-23 22:49:00 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            courseGoal :'Finish the course ',
+            vueLink : 'https://www.samanja.dev/'
+        };
+    }
+});
+
+app.mount('#user-goal')
